7cabe83b922bed53a9010c4b1ddfba44b837d3b2
Tofayel Ahmed Topu <53596291+killswitch99@users.noreply.github.com>
2022-02-20 17:05:56 +1100

    fix

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.resolve(__dirname, '/client/build')))
 	// All other GET requests not handled before will return our React app
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+		res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 	})
 } else {
 	app.get('/', (req, res) => {
